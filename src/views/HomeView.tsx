688c87b9cb6b2e7e2c5993ef00c35521b528e049
Omar <omarcory2@gmail.com>
2024-02-16 23:34:57 -0600

    Updated Home view

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -3,11 +3,16 @@ export const HomeView = () => {
     return (
         <div className="container">
             <div className="home-view">
-                <div className="home-info">
-                    <img src="src\assets\PORTRAIT.png"/>
-                    <h2>Hi! my name is <span>Omar</span></h2>
-                    <p>I'm a software developer</p>
-                    <button>Check Resume</button>
+                <div className="home-main">
+                    <div className="home-image">
+                        <img src="src\assets\PORTRAIT.png"/>
+                    </div>
+                    <div className="home-text">
+                        <p>Hi! my name is</p>
+                        <h2>Omar Villanueva</h2>
+                        <p className="summary">I'm a software developer, I desing and code elegant and efficient web applications</p>
+                        <button>Check Resume</button>
+                    </div>
                 </div>
             </div>
         </div>
