0f1cab103da41483486f8c0a1a48e20f713e0bc4
Omar <omarcory2@gmail.com>
2024-02-19 00:38:16 -0600

    Updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,14 @@
 
 export const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className="footer">
+            <div className="footer-images">
+                <img src="src\assets\icons\linkedin.png" alt="Linkedin" />
+                <img src="src\assets\icons\github.png" alt="Github" />
+                <img src="src\assets\icons\email.png" alt="Email" />
+            </div>
+            <p>Created by Omar Villanueva with 💛</p>
+            <p>All Rights Reserved © 2024</p>
+        </div>
     )
 }
