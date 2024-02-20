c8d9b6607909e240c93e15577407ce89fd547e1f
Omar <omarcory2@gmail.com>
2024-02-20 01:29:45 -0600

    Added about component

---
+++ src/components/About.tsx
@@ -0,0 +1,39 @@
+import '../styles/about.css';
+
+export const About = () => {
+    return (
+        <div id="about-view">
+            <div className="about-main">
+                <div className='about-image'>
+                    <img src="src\assets\PORTRAIT.png" alt="portrait"/>
+                </div>
+                <div className="about-text">
+                    <h2>ABOUT ME</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto dolorem a blanditiis obcaecati atque totam maxime illo corporis tempore enim laborum dolores soluta, debitis vero animi beatae sed porro ullam voluptate voluptatem dolorum! Libero magnam ullam veritatis minus dignissimos, impedit iusto vero id exercitationem quod voluptatum neque perspiciatis corrupti inventore?
+                    </p>
+                    <div className="about-lists">
+                        <div className="list">
+                            <h4>APTITUDES</h4>
+                            <ul>
+                                <li>Lorem ipsum dolorrrrrrrrr</li>
+                                <li>Lorem ipsum dolorqw</li>
+                                <li>Lorem ipsum dolorqweeeeeeeee</li>
+                                <li>Lorem ipsum </li>
+                            </ul>
+                        </div>
+                        <div className="list">
+                            <h4>CUALIDADES</h4>
+                            <ul>
+                                <li>Lorem ipsum dolor</li>
+                                <li>Lorem ipsum dolor</li>
+                                <li>Lorem ipsum dolor</li>
+                                <li>Lorem ipsum dolor</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
